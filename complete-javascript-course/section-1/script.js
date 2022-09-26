f31d677059221efb1c93c1fe15b1165d12450a9e
marcio1998 <marciogabriel1998@gmail.com>
2022-09-26 11:42:06 -0300

    Initial Commit

---
+++ complete-javascript-course/section-1/script.js
@@ -0,0 +1,34 @@
+let js = 'amazing';
+// if (js == 'amazing') alert('JavaScript IS FUN');
+console.log(40 + 8 + 23 - 10);
+console.log('Marcio');
+console.log(24);
+
+let firstName = 'Lavinia';
+console.log(firstName);
+
+let PI = 3.14;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'BI';
+
+console.log(myFirstJob);
+
+
+// ===== DATA TYPES =====
+
+let javaScriptIsfun = true;
+console.log(javaScriptIsfun);
+console.log(typeof(javaScriptIsfun));
+console.log(typeof(23));
+console.log(typeof('Márcio'));
+javaScriptIsfun = 'YES!';
+console.log(typeof(javaScriptIsfun));
+let year;
+console.log(typeof(year));
+year = 1998;
+console.log(typeof(year));
+console.log(typeof null);
+
+
+// ===== DECLARE VARIABLES =====
